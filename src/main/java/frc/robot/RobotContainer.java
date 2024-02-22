897f9e1e9fa1bbe172d4d5a7c659e473d8251d3e
oK-lol24 <borkdi@gmail.com>
2024-02-22 16:55:31 -0600

    Cont. Last

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -171,6 +171,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-  return new PathPlannerAuto("Intake Test 1");
+  return new PathPlannerAuto("Shooter Test 1");
   }
 }
