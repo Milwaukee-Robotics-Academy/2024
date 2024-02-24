68e52b303bbc4dc309aa2a0de936c9a96179392a
Laptop9 <laptop9@milwaukeeroboticsacademy.org>
2024-02-24 15:48:18 -0600

    EVENT

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,12 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST T and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
 public final class Constants {
+   public static final int kLeftClimberMotorPort  = 7;
+    public static final int kRightClimberMortorPort = 8;
   public static final class DriveConstants {
     public static final int kLeftMotorPort1 = 1;
     public static final int kLeftMotorPort2 = 2;
@@ -29,8 +31,16 @@ public final class Constants {
         // Assumes the encoders are directly mounted on the wheel shafts
         (kWheelDiameterInches * Math.PI) / (double) kEncoderCPR;
 
-    public static final int kForwardBackSlewRate = 3;
-    public static final int kTurnSlewRate = 2;
-    
+
+    public static final int kForwardBackSlewRate = 1;
+    public static final int kTurnSlewRate = 1;
+    public static final double kForwardBackSlewThreshold = 0; 
+    public static final double kTurnSlewThreshold = 1; // TODO change this
+
+    public static final double kTrackWidthMeters = 0.53;
+
+    public static final double kS = 0.11673; // copied from the features/pathplanner branch
+    public static final double kV = 1.3815;
+    public static final double kA = 0.12011;
   }
 }
